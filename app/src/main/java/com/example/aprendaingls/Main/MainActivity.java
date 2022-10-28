c482a9c937fb77ce40cbe93ae64ef8fe403bf4e2
israel.silva <israelpossidonio9@gmail.com>
2022-10-28 08:00:31 -0300

    #create interface, fragment_animals.xml, fragment_numbers.xml #include image, souds,colors

--- app/src/main/java/com/example/aprendaingls/Main/MainActivity.java
+++ app/src/main/java/com/example/aprendaingls/Main/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.aprendaingls.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.aprendaingls.Fragments.AnimalsFragment;
+import com.example.aprendaingls.Fragments.NumbersFragment;
+import com.example.aprendaingls.Fragments.VowelsFragment;
 import com.example.aprendaingls.R;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main );
 
         getSupportActionBar().setElevation( 0 );
+
+        smartTabLayout = findViewById( R.id.smartTabLayout );
+        viewPager = findViewById( R.id.viewPager );
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Animais", AnimalsFragment.class )
+                .add("Números", NumbersFragment.class )
+                .add("Vogais", VowelsFragment.class )
+                .create());
+
+        viewPager.setAdapter( adapter );
+        smartTabLayout.setViewPager(viewPager);
     }
+
 }
